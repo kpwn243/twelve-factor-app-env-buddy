3497dc86c260d95819654ecd587c18602c18a807
Jonathan Womack <jonathan@jwomack.com>
2021-02-03 11:52:27 -0600

    Fix entity unique constraints

--- internal/entities.go
+++ internal/entities.go
@@ -12,9 +12,9 @@ type Application struct {
 }
 
 type Environment struct {
-	ID                 uint `gorm:"primary_key"`
-	ApplicationId      uint
-	EnvName            string `gorm:"unique;not null"`
+	ID                 uint   `gorm:"primary_key"`
+	ApplicationId      uint   `gorm:"not null;index:env_unique"`
+	EnvName            string `gorm:"not null;index:env_unique"`
 	WriteWithoutPrefix bool   `gorm:"default:0;not null"`
 	Active             bool   `gorm:"default:1;not null"`
 	Variables          []Variable
@@ -23,10 +23,10 @@ type Environment struct {
 }
 
 type Variable struct {
-	ID            uint `gorm:"primary_key"`
-	EnvironmentId uint
-	VarName       string `gorm:"unique;not null"`
-	VarValue      string `gorm:"unique;not null"`
+	ID            uint   `gorm:"primary_key"`
+	EnvironmentId uint   `gorm:"not null;index:var_unique"`
+	VarName       string `gorm:"not null;index:var_unique"`
+	VarValue      string `gorm:"not null"`
 	Active        bool   `gorm:"default:1;not null"`
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
